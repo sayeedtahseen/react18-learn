94ed443d04a8288cd6ba5f8f30dad78eb2bbaf66
Sayeed Tahseen <sayeedtahseen@Sayeeds-MBP.attlocal.net>
2023-04-11 23:16:33 -0400

    use effect code

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,31 @@
 const url = 'https://api.github.com/users';
+import { useState, useEffect } from "react";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  
+  useEffect( () => {
+    const fetchUsers = async () => {
+      try{
+        const data = await fetch(url);
+        // const users = await data.json();
+        // console.log(users)
+        setUsers( await data.json());
+      } catch(error){
+        console.error(error);
+      }
+    }
+    
+    fetchUsers();
+  });
+  return (<>
+  <h2>fetch data example</h2>
+  {users.map((user, index) => {
+    return (<div key={index}>
+      {user.login}
+    </div>)
+  })}
+  </>);
 };
 export default FetchData;
